e935c6f6a29eff07da29445eb05bb9a1e0a0b3e5
Pbjsd <104098660+Pbjsd@users.noreply.github.com>
2024-11-08 00:38:07 -0500

    Added API Integration

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,32 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
+
         <footer>
           This project was coded by{" "}
-          <a href="https://sdhaliwalportfolio.com" target="_blank">
+          <a 
+            href="https://sdhaliwalportfolio.com" 
+            target="_blank"
+            rel="noopener noreferrer"
+            >
             Sandeep D.
           </a>{" "}
           and is{" "}
           <a 
             href="https://github.com/Pbjsd/react-weather-app"
             target="_blank"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a 
+            href="https://thatshecodesweatherapp.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify 
           </a>
         </footer>
       </div>
